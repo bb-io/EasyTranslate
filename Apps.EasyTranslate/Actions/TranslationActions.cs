45905932f9f742c9700234d9910a3b4edc96904b
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-04-29 00:10:28 +0300

    Latest changes

---
+++ Apps.EasyTranslate/Actions/TranslationActions.cs
@@ -0,0 +1,12 @@
+using Apps.EasyTranslate.Invocables;
+using Blackbird.Applications.Sdk.Common.Actions;
+using Blackbird.Applications.Sdk.Common.Invocation;
+
+namespace Apps.EasyTranslate.Actions;
+
+[ActionList]
+public class TranslationActions(InvocationContext invocationContext)
+    : AppInvocable(invocationContext)
+{
+    
+}
