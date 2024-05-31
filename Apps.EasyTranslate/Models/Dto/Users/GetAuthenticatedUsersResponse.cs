b694568e91bcfb012d7403807c108cc8043ebf34
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-05-31 18:01:09 +0300

    Fixed deserialization error

--- Apps.EasyTranslate/Models/Dto/Users/GetAuthenticatedUsersResponse.cs
+++ Apps.EasyTranslate/Models/Dto/Users/GetAuthenticatedUsersResponse.cs
@@ -1,4 +1,7 @@
 ﻿using Apps.EasyTranslate.Models.Dto.Generic;
+using Apps.EasyTranslate.Models.Dto.Languages;
+using Apps.EasyTranslate.Models.Dto.Workflows;
+using Newtonsoft.Json;
 
 namespace Apps.EasyTranslate.Models.Dto.Users;
 
@@ -6,7 +9,25 @@ public class GetAuthenticatedUsersResponse
 {
     public Data<UserAttributes> Data { get; set; }
 
-    public Data<AccountAttributes>[] Included { get; set; }
+    public Data<AuthenticatedAccountAttributes>[] Included { get; set; }
     
     public Meta Meta { get; set; }
+}
+
+public class AuthenticatedAccountAttributes
+{
+    [JsonProperty("company_name")]
+    public string CompanyName { get; set; }
+
+    [JsonProperty("company_email")]
+    public string CompanyEmail { get; set; }
+
+    [JsonProperty("team_identifier")]
+    public string TeamIdentifier { get; set; }
+
+    [JsonProperty("workflows")]
+    public Data<WorkflowAttributes>[] Workflows { get; set; }
+
+    [JsonProperty("language_pairs")]
+    public Dictionary<string, object> LanguagePairs { get; set; }
 }
